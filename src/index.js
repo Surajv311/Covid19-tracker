ee56c7dbcfe5e892767881529123fc724c2b6bbd
Surajv <vsuraj2405@gmail.com>
2020-06-27 20:09:12 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+ReactDOM.render(
+    <App />,
+  document.getElementById('root')
+);
+// you may check public/index.html
